2e1c2a0de1f53cdea21c3a0b42142fb55efe2fef
Tyler Hoover <80370906+tjayhoover@users.noreply.github.com>
2022-02-22 11:47:46 -0500

    Add files via upload

---
+++ header_files/locking.h
@@ -0,0 +1,23 @@
+#ifndef LOCKING_H
+#define LOCKING_H
+
+#include "room.h"
+
+class Locking : public Room {
+public:
+    Locking(const std::string& name);
+
+    std::weak_ptr<Room> take_door(Direction dir) const override;
+    std::pair<bool, std::string> can_move(Direction dir) const override;
+    bool can_adjacent(std::shared_ptr<Room> rm, Direction dir) const override;
+    void add_adjacent(std::shared_ptr<Room> rm, Direction dir) override;
+    bool is_door(Direction dir) const override;
+
+private:
+    std::weak_ptr<Room> east;
+    std::weak_ptr<Room> west;
+    std::weak_ptr<Room> north;
+    std::weak_ptr<Room> south;
+};
+
+#endif
